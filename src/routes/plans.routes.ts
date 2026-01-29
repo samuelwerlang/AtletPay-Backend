3d49c3d744d430b336e566c38b19087809f38ae8
Samuel Werlang <samuelwerlang13@gmail.com>
2026-01-29 19:22:02 -0300

    plansroutes big fixed

--- src/routes/plans.routes.ts
+++ src/routes/plans.routes.ts
@@ -8,16 +8,6 @@ import {
   deleteUserPlanController,
 } from "../controllers/userplans.controller.js";
 import requireAuth from "../middlewares/checkAuth.middleware.js";
-<<<<<<< HEAD
-
-const router = express.Router();
-
-router.post("/plan", jwtCheck, requireAuth, createUserPlanController);
-router.get("/plans", jwtCheck, requireAuth, getAllUserPlansController);
-router.get("/plan/:id", jwtCheck, requireAuth, getUserPlanController);
-router.patch("/plan/:id", jwtCheck, requireAuth, updateUserPlanController);
-router.delete("/plan/:id", jwtCheck, requireAuth, deleteUserPlanController);
-=======
 import getCurrentUser from "../middlewares/getCurrentUser.middleware.js";
 
 const router = express.Router();
@@ -57,6 +47,5 @@ router.delete(
   getCurrentUser,
   deleteUserPlanController,
 );
->>>>>>> backend-setup
 
 export default router;
